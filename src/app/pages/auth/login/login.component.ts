bdd0999669dead9f3469aa557a644ceb39bc5c9f
Kagan Cubukcu <kagancancubukcu@gmail.com>
2024-10-01 13:12:48 +0300

    improved user interface experience in the login and signup pages

--- src/app/pages/auth/login/login.component.ts
+++ src/app/pages/auth/login/login.component.ts
@@ -14,39 +14,34 @@ import { Router } from '@angular/router';
 export class LoginComponent implements OnInit {
   loginForm: FormGroup;
 
-  constructor(private readonly fb: FormBuilder, private readonly store: Store<AppStateInterface>, private readonly router: Router) {
+  constructor(
+    private readonly fb: FormBuilder,
+    private readonly store: Store<AppStateInterface>,
+    private readonly router: Router
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: '',
+      password: ['', [Validators.required]],
     });
   }
 
   ngOnInit() {
-    //After login, if the user is logged in, navigate to the home page
     this.store.select(AuthSelectors.loggedInUserSelector).subscribe((loggedInUser) => {
-        if (loggedInUser) this.router.navigate(['/']);
-      });
+      if (loggedInUser) this.router.navigate(['/']);
+    });
   }
 
   onSubmit() {
     const email = this.loginForm.get('email')?.value;
     const password = this.loginForm.get('password')?.value;
-    //Check user credentials
     this.store.select(AuthSelectors.loggedInUserSelector).subscribe((loggedInUser) => {
-        //If the user is not logged in, set the error message
-        if (!loggedInUser) {
-          this.loginForm.get('errorMessage')?.setValue('Invalid Email or Password');
-        } else {
-          //If the user is logged in, set the error message to an empty string
-          this.loginForm.get('errorMessage')?.setValue('');
-        }
-      });
+      if (!loggedInUser) {
+        this.loginForm.get('errorMessage')?.setValue('Invalid Email or Password');
+      } else {
+        this.loginForm.get('errorMessage')?.setValue('');
+      }
+    });
 
-    //Dispatch login action
     this.store.dispatch(AuthActions.login({ email, password }));
-  }
-
-  backToHome() {
-    this.router.navigate(['/']);
   }
 }
