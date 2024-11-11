387108f25509ed2f3c70aa8c1c2d0252a6c7a3f1
Kagan Cubukcu <kagancancubukcu@gmail.com>
2024-11-11 13:20:42 +0300

    modernize footer design with responsive layout and improved mobile support

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -7,10 +7,29 @@ import { RouterModule } from '@angular/router';
 import { LogoPipe } from './pipe/logo.pipe';
 import { FormErrorDirective } from './directives/form-error.directive';
 import { DateFormatPipe } from './pipe/date-format.pipe';
-
+import { FooterSectionComponent } from '../features/footer/footer-section/footer-section.component';
+import { CollapsibleSectionComponent } from '../features/footer/collapsible-section/collapsible-section.component';
 @NgModule({
-  declarations: [HeaderComponent, NavbarComponent, FooterComponent, LogoPipe, FormErrorDirective, DateFormatPipe],
+  declarations: [
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    LogoPipe,
+    FormErrorDirective,
+    DateFormatPipe,
+    CollapsibleSectionComponent,
+    FooterSectionComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [HeaderComponent, NavbarComponent, FooterComponent, LogoPipe, FormErrorDirective, DateFormatPipe],
+  exports: [
+    HeaderComponent,
+    NavbarComponent,
+    FooterComponent,
+    LogoPipe,
+    FormErrorDirective,
+    DateFormatPipe,
+    CollapsibleSectionComponent,
+    FooterSectionComponent,
+  ],
 })
 export class SharedModule {}
