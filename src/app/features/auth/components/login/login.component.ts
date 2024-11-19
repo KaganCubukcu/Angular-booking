504cbe29a7e89a7e7e150db3c2ff27554894559a
Kagan Cubukcu <kagancancubukcu@gmail.com>
2024-11-19 12:35:48 +0300

    improve login component

--- src/app/features/auth/components/login/login.component.ts
+++ src/app/features/auth/components/login/login.component.ts
@@ -1,47 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { Subject, takeUntil } from 'rxjs';
 import * as AuthActions from '../../store/auth.actions';
 import * as AuthSelectors from '../../store/auth.selectors';
-import { AppStateInterface } from '../../../../core/models/app-state.model';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
+  styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm: FormGroup;
+  loading$ = this.store.select(AuthSelectors.selectLoading);
+  error$ = this.store.select(AuthSelectors.selectError);
+  private destroy$ = new Subject<void>();
 
   constructor(
-    private readonly fb: FormBuilder,
-    private readonly store: Store<AppStateInterface>,
-    private readonly router: Router
+    private fb: FormBuilder,
+    private store: Store
   ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
     });
   }
 
-  ngOnInit() {
-    this.store.select(AuthSelectors.loggedInUserSelector).subscribe((loggedInUser) => {
-      if (loggedInUser) this.router.navigate(['/']);
-    });
-  }
-
-  onSubmit() {
-    const email = this.loginForm.get('email')?.value;
-    const password = this.loginForm.get('password')?.value;
-    this.store.select(AuthSelectors.loggedInUserSelector).subscribe((loggedInUser) => {
-      if (!loggedInUser) {
-        this.loginForm.get('errorMessage')?.setValue('Invalid Email or Password');
-      } else {
-        this.loginForm.get('errorMessage')?.setValue('');
+  ngOnInit(): void {
+    // Subscribe to error changes to handle form state
+    this.error$.pipe(
+      takeUntil(this.destroy$)
+    ).subscribe(error => {
+      if (error) {
+        this.loginForm.setErrors({ serverError: error });
       }
     });
+  }
 
-    this.store.dispatch(AuthActions.login({ email, password }));
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      this.store.dispatch(AuthActions.login({ email, password }));
+    } else {
+      this.markFormGroupTouched(this.loginForm);
+    }
+  }
+
+  private markFormGroupTouched(formGroup: FormGroup): void {
+    Object.values(formGroup.controls).forEach(control => {
+      control.markAsTouched();
+
+      if (control instanceof FormGroup) {
+        this.markFormGroupTouched(control);
+      }
+    });
+  }
+
+  getErrorMessage(controlName: string): string {
+    const control = this.loginForm.get(controlName);
+    
+    if (control?.hasError('required')) {
+      return `${controlName.charAt(0).toUpperCase() + controlName.slice(1)} is required`;
+    }
+    
+    if (control?.hasError('email')) {
+      return 'Please enter a valid email address';
+    }
+    
+    if (control?.hasError('minlength')) {
+      return `${controlName.charAt(0).toUpperCase() + controlName.slice(1)} must be at least ${control.getError('minlength').requiredLength} characters`;
+    }
+
+    return '';
   }
 }
