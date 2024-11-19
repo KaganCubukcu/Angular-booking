2bd0654067c5e6bddc39fc487a02075f717ef6ac
Kağan Çubukçu <99168337+KaganCubukcu@users.noreply.github.com>
2024-11-19 06:03:46 +0000

    wip

--- src/environments/environments.ts
+++ src/environments/environments.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  stripeKey:
-    'pk_test_51LbKwOG2h6tSgHSJsTZs25YcneRmo9bqxinNowOQHUXelwFY6Yfigbg18RYpSQlOji0zLqMYR1dOfA00qb1DZtXg00ol341evG',
+  stripeKey: "strip_key",
+  apiUrl: 'http://localhost:8000'
 };
